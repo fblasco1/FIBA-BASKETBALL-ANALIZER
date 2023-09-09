1ea9ac79a7778d4df28c40825edfd6ca338e31bd
Francisco Blasco <francisco.o.blasco@gmail.com>
2023-09-09 19:45:50 -0300

    Corregido app.py

--- src/app.py
+++ src/app.py
@@ -8,6 +8,7 @@ app = Dash(__name__,
            suppress_callback_exceptions=True, 
            meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}]
     )
+server = app.server
 
 navbar = html.Nav([
                 dbc.Container([
